08da5ee68857bf63dc344fd786779614c5c7a35d
Ilya Shusterman <ilyashusterman@outlook.com>
2017-02-23 22:36:11 +0200

    framework rdy

---
+++ Util.py
@@ -0,0 +1,21 @@
+
+videos_MAX =10000
+endpoint=1000
+request_descriptions=1000000
+cache_servers=1000
+cache_server_capacity=500000
+
+
+# optimize how videos is delivered to the users
+
+
+# check popularity function
+
+
+# cache store video to cache servers
+
+def store_video(video):
+    # TODO video storage to cache server - database implementation
+    print('Done storing video to some decided cache server')
+
+# optimize distribute to cache servers based on popularity
