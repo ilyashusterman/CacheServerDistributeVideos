81ed1d9ea72127e831e97ab5ce2a24053ed94973
Ilya Shusterman <ilyashusterman@outlook.com>
2017-02-23 23:29:38 +0200

    framework rdy

--- Util.py
+++ Util.py
@@ -1,20 +1,41 @@
-
-videos_MAX =10000
-endpoint=1000
-request_descriptions=1000000
-cache_servers=1000
-cache_server_capacity=500000
+videos_MAX = 10000
+endpoint = 1000
+request_descriptions = 1000000
+cache_servers = 1000
+x_cache_server_capacity = 500000
 
 
 # optimize how videos is delivered to the users
 
 
-# check popularity function
+# check popularity function by request
+def calc_popularity(requests, endpoints):
+    # TODO video storage to cache server - database implementation
+    # popular video by requests and endpoints
+    video = {}
+    print('Done popular video')
+    return video
+
+
+def can_video_be_store(video ,cacheserver):
+   return video.size <= cacheserver.size
 
 
 # cache store video to cache servers
+def make_store(video, cacheserver):
+    #TODO make video store to cachserver
+    pass
 
-def store_video(video):
+
+def store_video(videos, cacheservers):
+    #for each cache server in cache servers
+    for cacheserver in cacheservers:
+        #for each video in videos
+        for video in videos:
+            if can_video_be_store(video, cacheserver):
+                make_store(video, cacheserver)
+
+
     # TODO video storage to cache server - database implementation
     print('Done storing video to some decided cache server')
 
--- Video.py
+++ Video.py
@@ -11,12 +11,14 @@ class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     views = db.Column(db.Integer)
     name = db.Column(db.String(120), unique=True)
+    size = db.Column(db.Integer)
 
-    def __init__(self, views, name, pub_date=None):
+    def __init__(self, views, name, size, pub_date=None):
         if pub_date is None:
             pub_date = datetime.utcnow()
         self.views = views
         self.name = name
+        self.size = size
 
     def __repr__(self):
         return '<User %r>' % self.name
